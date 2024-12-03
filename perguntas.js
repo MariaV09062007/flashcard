b739279e7104c6b59cb95d669cc560bb5684a2d3
MariaV09062007 <00001117423979sp@al.educacao.sp.gov.br>
2024-12-03 12:07:55 -0300

    add files

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'charada',
+    'O que é, o que é? Feito para andar e não anda.?',
+    'A rua.'
+)
+
+criaCartao(
+    'charada',
+    ' O que é, o que é? Dá muitas voltas e não sai do lugar.',
+    'O relógio.'
+)
+
+criaCartao(
+    'charada',
+    'O que é, o que é? Tem cabeça e tem dente, não é bicho e nem é gente',
+    'O alho.'
+)
+
+criaCartao(
+    'charada',
+    ' O que é, o que é? Não se come, mas é bom para se comer.?',
+    'O talher'
+)
